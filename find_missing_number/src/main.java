0d3038e40e3b1434277586e9cc4f0727f6ef0c92
Shlomi <vgtvgy1@gmail.com>
2022-02-11 23:33:01 +0200

    find missing number

---
+++ find_missing_number/src/main.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class main {
+    public int findMissingNumber(int[] input, int n) {
+        //Note: input size is never zero.
+
+        //Get sum of complete array (with missing number X)
+        int sum = n * (n + 1);
+        sum /= 2;
+
+        for(int num : input) {
+            sum -= num;
+        }
+        return sum;
+    }
+
+    @Test
+    public void test() {
+        int arr[] = {3, 7, 1, 2, 8, 4, 5};
+        assertEquals(6, findMissingNumber(arr, 8));
+
+        int arr2[] = {3, 7, 1, 2, 8, 4, 6};
+        assertEquals(5, findMissingNumber(arr2, 8));
+
+        int arr3[] = {1, 3};
+        assertEquals(2, findMissingNumber(arr3, 3));
+
+        int arr4[] = {1};
+        assertEquals(2, findMissingNumber(arr4, 2));
+    }
+}
